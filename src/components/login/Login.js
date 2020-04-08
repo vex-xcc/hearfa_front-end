18a117a23aef3b2f4f2ad020cad51a3d22d4fb50
Ralabdulwa98 <rmalabdulwahab@gmail.com>
2020-04-08 11:56:59 +0300

    add register button to use call register component

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -2,7 +2,9 @@ import React, { Component } from "react";
 import axios from "axios";
 import apiURL from '../../APIconfig';
 import { getInfo } from "./decodeToken";
+import { Route, BrowserRouter, Link } from "react-router-dom";
 import "./login.css";
+import Register from './Register'
 class Login extends Component {
   constructor(props) {
     super(props);
@@ -36,47 +38,62 @@ class Login extends Component {
         localStorage.setItem("currentUser", res.data.token);
         let jwt1 = getInfo().data.Worker;
         if (jwt1 === true) {
-          history.push("/WorkerHeader");
+          console.log("this.props.history:", this.props);       
+          this.props.history.push("/WorkerHeader");
+          
         } else if (jwt1 === false) {
-           history.push("/CustomerHeader");
+          console.log("B:", jwt1);
+          this.props.history.push("/CustomerHeader");
+
         } else if (jwt1 === undefined) {
-          history.push("/");
+          console.log("b: ", jwt1);
+          this.props.history.push("/");
         }
         return res;
       })
       .catch(error => {
         console.log("ERROR: ", error);
+
       });
   }
 
   render() {
 
     return (
+      <BrowserRouter>
       <div>
-
-        <form className="login" onSubmit={e => this.submit(e)} >
-          <input
-            type="text"
-            name="Username"
-            onChange={e => this.change(e)}
-            value={this.state.Username}
-          />
-          <input
-            type="password"
-            placeholder="password"
-            name="password"
-            onChange={e => this.change(e)}
-            value={this.state.password}
-          />
+    
+        <form className="login" onSubmit={e => this.submit(e)} >     
+        <input
+              type="text"
+              name="Username"
+              onChange={e => this.change(e)}
+              value={this.state.Username}
+            />
+            <input
+              type="password"
+              placeholder="password"
+              name="password"
+              onChange={e => this.change(e)}
+              value={this.state.password}
+            />
           <button
             type="submit" >Login</button>
-          <br></br>
-
+            <br></br>
+            <button><Link to="/register">Register </Link> </button>
+            
+        
+             
+             
         </form>
         <h2>&nbsp;</h2>
 
       </div>
-
+      <Route
+          path="/register"
+          render={() => <Register  history={this.props.history}  />}
+        />
+      </BrowserRouter>
     );
   }
 }
