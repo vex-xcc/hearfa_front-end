b6428741b08c8a02a2220365c08d977db99c581a
Ralabdulwa98 <rmalabdulwahab@gmail.com>
2020-04-08 23:38:28 +0300

    add development url

--- src/APIconfig.js
+++ src/APIconfig.js
@@ -4,7 +4,7 @@ let apiURL ;
 const expressPort = 5000;
 const apiUrls = {
     development:`http://localhost:${expressPort}/api`,
-    production:`https://enjaz-0011.herokuapp.com/api`,
+    production:`https://hearfa.herokuapp.com/api`,
 } 
 
 if( window.location.hostname === 'localhost' ){
